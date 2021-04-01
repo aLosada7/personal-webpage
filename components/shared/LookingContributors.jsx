5e9b29bffd13842d59829952bf5adcd4266f722b
Alvaro <aldc30sc@gmail.com>
2021-04-01 23:36:47 +0200

    Place up added to my styles

--- components/shared/LookingContributors.jsx
+++ components/shared/LookingContributors.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function LookingContributors() {
     return (
-        <div className="tags-container tags-black mt-8">
+        <div className="tags-container tags-green mt-8 place-up">
             <Link href="/contact"><a role="link" target="_blank" aria-label="looking-new-contributors">Looking for new contributors!</a></Link>
         </div>
     )
