ef72dda38dbfa44d60281be5e2e18f2e9394889d
Alvaro <aldc30sc@gmail.com>
2022-02-14 11:27:49 +0100

    feat: :sparkles: valentines day

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -19,14 +19,36 @@ export default function Blog() {
 			</section>
 			<section className="page-section py-section">
 				<div className="container">
-					<div className="col-16 offset-4 col-mobile-24 col-mobile-offset-0">
-						<p className="text-center">
-							<strong>
-								I am working on it and hope it will be available soon. <br />
-								Come later for more updates :)
-							</strong>
-						</p>
-					</div>
+					{
+						<div className="row blog-summary-entry-list">
+							{blogs.map((blog) => (
+								<div className="col-8 col-offset-0 col-mobile-24" key={blog.slug}>
+									<div className="blog-summary-entry mb-3">
+										<article className="blog-entry">
+											<img
+												className="mb-8"
+												src={`/blogs/${blog.slug}.jpeg`}
+												alt={blog.headline}
+											/>
+											<span className="blog-entry-date mb-8">{blog.date}</span>
+
+											<Link href={{ pathname: `/blog/${blog.slug}` }}>
+												<strong className="blog-entry-title">
+													<a>{blog.headline}</a>
+												</strong>
+											</Link>
+
+											<p>{blog.description}</p>
+
+											<Link href={{ pathname: `/blog/${blog.slug}` }}>
+												<a className="blog-entry-read-more">Read More</a>
+											</Link>
+										</article>
+									</div>
+								</div>
+							))}
+						</div>
+					}
 				</div>
 			</section>
 		</Layout>
--- pages/san-valentine.jsx
+++ pages/san-valentine.jsx
@@ -4,10 +4,39 @@ export default function SanValentine() {
 	return (
 		<>
 			<div className="sv-slide">
+				<h1 class="gradient-text" style={{ fontSize: "15vmin" }}>
+					I wish you a very happy San Valentine's Day!
+				</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text">I have a special suprise for you!</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text" style={{ fontSize: "12vmin" }}>
+					Are you ready? Keep scrolling and enjoy!
+				</h1>
+			</div>
+			<div className="sv-slide">
 				<h1 class="gradient-text">Here we go!</h1>
-				<span className="scrollmouse-wrapper">
-					<ScrollMouse />
-				</span>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text">We are going to make a trip!</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text">Do you guess which place are we visiting?</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text">It's time to know</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text" style={{ fontSize: "20vmin" }}>
+					Prepare yourself!
+				</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text" style={{ fontSize: "15vmin" }}>
+					This March we are travelling to...
+				</h1>
 			</div>
 			<div className="sv-slide">
 				<h1 class="gradient-text" style={{ fontSize: "12vmin" }}>
@@ -21,6 +50,26 @@ export default function SanValentine() {
 				<h1 class="gradient-text" style={{ fontSize: "10vmin" }}>
 					It's going to be a great weekend!
 				</h1>
+			</div>
+			<div className="sv-slide" style={{ flexDirection: "column" }}>
+				<h1 class="gradient-text" style={{ fontSize: "10vmin" }}>
+					Be prepared to visit
+				</h1>
+				<ul style={{ padding: "0" }}>
+					<li>The British Museum</li>
+					<li>Hyde Park</li>
+					<li>Abbey Road</li>
+					<li>Chinatown</li>
+					<li>Leicester Square</li>
+					<li>Buckinham Palace</li>
+					<li>Westminster</li>
+					<li>Soho</li>
+					<li>Thames River</li>
+					<li>Picadilly Circus</li>
+					<li>Oxford Street</li>
+					<li>Leicester Square</li>
+					<li>And many more...</li>
+				</ul>
 			</div>
 		</>
 	);
