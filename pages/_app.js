155f9e56c9fdf40309c1a8bf38f69606d0bef1e0
Alvaro <aldc30sc@gmail.com>
2022-02-17 08:45:19 +0100

    feat: :heavy_plus_sign: Introduces dana-react

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,16 @@
-import '../styles/globals.scss'
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+
+import "../styles/globals.scss";
+
+const key = "custom";
+const cache = createCache({ key });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<CacheProvider value={cache}>
+			<Component {...pageProps} />
+		</CacheProvider>
+	);
 }
-export default MyApp
+export default MyApp;
