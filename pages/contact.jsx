4723ef772155cf9f30d40e61ca57268a40984116
Alvaro <aldc30sc@gmail.com>
2022-03-17 18:39:52 +0100

    refactor: :lipstick: Max-width on large text

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,3 +1,5 @@
+import { Text } from "dana-react";
+
 import Layout from "../components/Layout";
 
 export default function Contact() {
@@ -15,15 +17,11 @@ export default function Contact() {
 				</div>
 			</section>
 			<section className="page-section">
-				<div className="container contact">
-					<div className="row">
-						<div className="col-24">
-							<h3 className="text-center">
-								Get in touch through my social media, or sending a message to my e-mail{" "}
-								<i>(aldc30sc@gmail.com)</i>
-							</h3>
-						</div>
-					</div>
+				<div className="container contact" style={{ maxWidth: "34em", textAlign: "center" }}>
+					<Text>
+						Get in touch through my social media, or sending a message to my e-mail{" "}
+						<i>(aldc30sc@gmail.com)</i>
+					</Text>
 				</div>
 			</section>
 		</Layout>
