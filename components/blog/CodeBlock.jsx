6728ab9bed47a4d952e9ab7528495b44ecfd78be
Alvaro <aldc30sc@gmail.com>
2022-11-19 16:51:43 +0100

    blog improvements

--- components/blog/CodeBlock.jsx
+++ components/blog/CodeBlock.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Highlight, { defaultProps } from "prism-react-renderer";
-import theme from "prism-react-renderer/themes/nightOwl";
+import theme from "prism-react-renderer/themes/vsDark";
 
 const CodeBlock = ({ children, className }) => {
 	const language = className.replace(/language-/, "");
