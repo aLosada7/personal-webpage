9d4836bc894d11c7c73d002db422188a4f093f26
aLosada7 <aldc30sc@gmail.com>
2023-11-15 16:16:13 +0100

    feat: n26

--- pages/links.jsx
+++ pages/links.jsx
@@ -27,6 +27,9 @@ const Links = () => {
 					<Button size="small" as="a" href="https://app.voltio.com/iMut/n6lsaz9r" target="_blank">
 						20€ gratis en Voltio con el código ALVAROL5
 					</Button>
+					<Button size="small" as="a" href="https://n26.com/r/alvarol9848" target="_blank">
+						10€ gratis en N26 si te abres una cuenta
+					</Button>
 				</ThemeProvider>
 			</Row>
 		</Row>
