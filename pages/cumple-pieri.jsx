6b4a3be609514c978d66228bea2d8cfa1102dab4
aLosada7 <aldc30sc@gmail.com>
2024-02-03 12:24:44 +0100

    cumple pieri

---
+++ pages/cumple-pieri.jsx
@@ -0,0 +1,88 @@
+import ScrollMouse from "../components/ScrollMouse";
+
+export default function CumplePieri() {
+	return (
+		<div style={{ width: "min-content" }}>
+			<div className="sv-slide">
+				<h1 class="gradient-text">¡¡¡Hola Pieri!!! Y a todos los que estais ahí</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text">¡Lo primero de todo!</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text">Feliz cumpleañosssssss</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text">¡Y que cumplas muchos más!</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text">Hoy te hemos preparado una sorpresa</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text">Queremos que pienses en un plan que nos has dicho que te gustaría hacer</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text">¿Adivinas que puede ser?</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text">Vamos a ver si has adivinado</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text" style={{ fontSize: "20vmin" }}>
+					¿Estas preparada?
+				</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text">¡Queremos regalarte una experiencia en un escape room!</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text" style={{ fontSize: "12vmin" }}>
+					Ahora es tu turno
+				</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text">Como no cuesta mucho decantarnos por uno</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text">Te proponemos estos 3 para que elijas uno</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text">
+					El primero se llama{" "}
+					<a href="https://locktopusescape.com/el-sastre/" target="_blank">
+						El Sastre
+					</a>
+				</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text">
+					Si ese no te convence, te proponemos{" "}
+					<a href="https://cubickmadrid.es/la-entrevista/" target="_blank">
+						La entrevista
+					</a>
+				</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text">
+					Y el último es{" "}
+					<a href="https://www.actionhouse.es/es/juegos/juegos-de-escape/rdb-menu" target="_blank">
+						Ratas de biblioteca
+					</a>
+				</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text">En honor a Gigi y Lea</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text" style={{ fontSize: "10vmin" }}>
+					¡El regalo es para dos personas, pero sientete libre de invitar a más amigos!
+				</h1>
+			</div>
+			<div className="sv-slide">
+				<h1 class="gradient-text" style={{ fontSize: "10vmin" }}>
+					Y recuerda decirnoslo cuando sepas la fecha y el número de personas
+				</h1>
+			</div>
+		</div>
+	);
+}
